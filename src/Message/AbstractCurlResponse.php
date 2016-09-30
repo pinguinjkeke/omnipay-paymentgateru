6b7f5b7a8ed8065df22f56e080305625b28cf0f5
pinguinjkeke <yaavakov@gmail.com>
2016-09-30 16:12:57 +0300

    AbstractCurlResponse

---
+++ src/Message/AbstractCurlResponse.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Omnipay\PaymentgateRu\Message;
+
+use Omnipay\Common\Message\AbstractResponse;
+use Omnipay\Common\Message\RequestInterface;
+
+class AbstractCurlResponse extends AbstractResponse
+{
+    /**
+     * Constructor
+     *
+     * @param RequestInterface $request the initiating request.
+     * @param mixed $data
+     */
+    public function __construct(RequestInterface $request, $data)
+    {
+        parent::__construct($request, json_decode($data, true));
+    }
+
+    /**
+     * Is the response successful?
+     *
+     * @return boolean
+     */
+    public function isSuccessful()
+    {
+        return !array_key_exists('errorCode', $this->data);
+    }
+
+    /**
+     * Response code
+     *
+     * @return null|string A response code from the payment gateway
+     */
+    public function getCode()
+    {
+        return $this->isSuccessful() ? null : $this->data['errorCode'];
+    }
+
+    /**
+     * Response Message
+     *
+     * @return null|string A response message from the payment gateway
+     */
+    public function getMessage()
+    {
+        return $this->isSuccessful() ? null : $this->data['errorMessage'];
+    }
+}
